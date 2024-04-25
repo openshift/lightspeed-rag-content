5e127146bfc82245743e3db74c79321ea397b5f5
Sergey Yedrikov <syedriko@redhat.com>
2024-04-25 16:01:13 -0400

    Ability to exclude OCP doc pages from RAG

--- scripts/get_ocp_plaintext_docs.sh
+++ scripts/get_ocp_plaintext_docs.sh
@@ -12,3 +12,7 @@ git clone --single-branch --branch enterprise-${OCP_VERSION} https://github.com/
 echo "product-version: $OCP_VERSION" >> scripts/asciidoctor-text/attributes.yaml
 
 python scripts/asciidoctor-text/convert-it-all.py -i openshift-docs -t openshift-docs/_topic_maps/_topic_map.yml -d openshift-enterprise -o ocp-product-docs-plaintext/${OCP_VERSION} -a scripts/asciidoctor-text/attributes.yaml
+
+for f in $(cat config/exclude.conf); do
+    rm ocp-product-docs-plaintext/${OCP_VERSION}/$f
+done
