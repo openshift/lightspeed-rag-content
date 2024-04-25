8a2c9415ba003b9b42a3f6dfeb69d662aa088864
Sergey Yedrikov <syedriko@redhat.com>
2024-04-24 21:52:27 -0400

    Added support for node lookup by node id

--- scripts/query_rag.py
+++ scripts/query_rag.py
@@ -5,7 +5,6 @@ import os
 
 from llama_index.core.storage.storage_context import StorageContext
 from llama_index.core import Settings, load_index_from_storage
-from llama_index.core.schema import NodeWithScore
 from llama_index.vector_stores.faiss import FaissVectorStore
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 
@@ -23,10 +22,11 @@ if __name__ == "__main__":
     parser.add_argument(
         "-m", "--model-path", required=True, help="path to the embedding model"
     )
-    parser.add_argument("-q", "--query", required=True, help="query to run")
+    parser.add_argument("-q", "--query", help="query to run")
     parser.add_argument(
-        "-k", "--top-k", type=int, required=True, help="similarity_top_k"
+        "-k", "--top-k", type=int, help="similarity_top_k"
     )
+    parser.add_argument("-n", "--node", help="retrieve node")
     args = parser.parse_args()
 
     os.environ["TRANSFORMERS_CACHE"] = args.model_path
@@ -35,13 +35,17 @@ if __name__ == "__main__":
     Settings.llm = None
     Settings.embed_model = HuggingFaceEmbedding(model_name=args.model_path)
 
+    storage_context=StorageContext.from_defaults(
+        vector_store=FaissVectorStore.from_persist_dir(args.db_path),
+        persist_dir=args.db_path,
+    )
     vector_index = load_index_from_storage(
-        storage_context=StorageContext.from_defaults(
-            vector_store=FaissVectorStore.from_persist_dir(args.db_path),
-            persist_dir=args.db_path,
-        ),
+        storage_context=storage_context,
         index_id=args.product_index,
     )
-    retriever = vector_index.as_retriever(similarity_top_k=args.top_k)
-    for n in retriever.retrieve(args.query):
-        print(n.__repr__())
+    if args.node is not None:
+        print(storage_context.docstore.get_node(args.node).__repr__())
+    else:
+        retriever = vector_index.as_retriever(similarity_top_k=args.top_k)
+        for n in retriever.retrieve(args.query):
+            print(n.__repr__())
