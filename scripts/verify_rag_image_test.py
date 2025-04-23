9d84a6611d3f2dda72cb84696dfa10b47b3064ad
Maxim Svistunov <makssvistunov@gmail.com>
2025-04-23 17:05:34 +0200

    Formatting improvements in verify_rag_image_test.py

--- scripts/verify_rag_image_test.py
+++ scripts/verify_rag_image_test.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     for version in versions:
         path = f"/rag/vector_db/ocp_product_docs/{version}/index_store.json"
         assert os.path.isfile(path)
-        
+
         path = "/rag/embeddings_model/config.json"
         assert os.path.isfile(path)
     print("Success")
