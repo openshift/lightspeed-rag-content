c584a4d69f28ed9fa208bf322544e61fa15c7409
Sergey Yedrikov <syedriko@redhat.com>
2025-09-25 20:18:25 -0400

    OLS-2033: Create Faiss Vector Store for OLS using l-core

---
+++ lsc/src/lightspeed_rag_content/asciidoc/__init__.py
@@ -0,0 +1,5 @@
+"""A package that can be used to process AsciiDoc formatted files."""
+
+from lightspeed_rag_content.asciidoc.asciidoctor_converter import AsciidoctorConverter
+
+__all__ = ["AsciidoctorConverter"]
